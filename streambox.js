5e4664906249e9cd430f1ee20788e29993cce99a
zeobuilder10 <adrienclemenceau1@gmail.com>
2021-10-05 13:12:35 +0200

    malding for ex02

--- streambox.js
+++ streambox.js
@@ -9,6 +9,14 @@ function duplicate(filename) {
     fs.createReadStream(filename).pipe(fs.createWriteStream(path.parse(filename).name + '.copy' + path.parse(filename).ext))
 }
 
+// .ex02 //
+
+function transform(filename, re, fn) {
+    fs.readFile(filename, 'utf-8', function() {
+        
+    }
+}
+
 // function export //
 
-module.exports = { duplicate }
+module.exports = { duplicate, transform }
